755a7d77b76ca2f489683d216faf739c56734a87
Zeeb-Bip <nazibshahab@gmail.com>
2026-06-16 21:53:19 +0700

    fix: remove missing lib aliases from dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     debugImplementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.animation)
